ed29602c664da8655f8db0a81867e581f37be20d
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-09-11 12:39:44 +0200

    Add: Empty Commands and Queries to all Application Features for now

---
+++ UniManagementSystem.Application/Features/AdminAccountRoles/Commands/DeleteAdminAccountRoleCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniManagementSystem.Application.Features.AdminAccountRoles.Commands
+{
+    public class DeleteAdminAccountRoleCommand
+    {
+    }
+}
---
+++ UniManagementSystem.Application/Features/AdminAccounts/Queries/GetAllAdminAccountQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniManagementSystem.Application.Features.AdminAccounts.Queries
+{
+    public class GetAllAdminAccountQuery
+    {
+    }
+}
