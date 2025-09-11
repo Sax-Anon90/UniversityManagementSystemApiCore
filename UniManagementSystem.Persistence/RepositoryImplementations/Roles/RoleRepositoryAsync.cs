7f2d735c426e905a35c5f674ea8a843a5fd37ed2
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-09-11 11:21:20 +0200

    Complete: Roles Repository

--- UniManagementSystem.Persistence/RepositoryImplementations/Roles/RoleRepositoryAsync.cs
+++ UniManagementSystem.Persistence/RepositoryImplementations/Roles/RoleRepositoryAsync.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,33 +7,75 @@ using System.Threading.Tasks;
 using UniManagementSystem.Application.RepositoryInterfaces.Roles;
 using UniManagementSystem.Data.DomainEntities;
 using UniManagementSystem.Data.DomainModels.ViewModels.Roles;
+using UniManagementSystem.Persistence.UniDbContext;
 
 namespace UniManagementSystem.Persistence.RepositoryImplementations.Roles
 {
     public class RoleRepositoryAsync : IRoleRepositoryAsync
     {
-        public RoleRepositoryAsync()
+        private readonly UniManagementSystemDbContext _dbContext;
+        public RoleRepositoryAsync(UniManagementSystemDbContext _dbContext)
         {
+            this._dbContext = _dbContext;
+        }
+        public async Task<Role> CreateRoleAsync(Role roleToCreate)
+        {
+            await _dbContext.Roles.AddAsync(roleToCreate);
+            await _dbContext.SaveChangesAsync();
+            return roleToCreate;
+        }
+
+        public async Task<Role> DeleteRoleAsync(Role roleToDelete)
+        {
+            var role = await _dbContext.Roles.AsNoTracking().SingleOrDefaultAsync(x => x.Id == roleToDelete.Id);
+
+            if (role is null)
+            {
+                return new Role() { Id = 0 };
+            }
+
+            role.IsActive = false;
+            role.DateInactive = DateTime.UtcNow;
+            _dbContext.Update(role);
+            await _dbContext.SaveChangesAsync();
+            return role;
+        }
+
+        public async Task<IEnumerable<RoleViewModel>> GetAllRolesAsync()
+        {
+            var roles = await _dbContext.Roles.Select(x => new RoleViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                IsActive = x.IsActive,
+                DateCreated = x.DateCreated,
+                DateModified = x.DateModified,
+                DateInactive = x.DateInactive,
+            }).ToListAsync();
+
+            if (!roles.Any())
+            {
+                return Enumerable.Empty<RoleViewModel>();
+            }
+
+            return roles;
+        }
+
+        public async Task<Role> UpdateRoleAsync(Role roleToUpdate)
+        {
+            var role = await _dbContext.Roles.AsNoTracking().SingleOrDefaultAsync(x => x.Id == roleToUpdate.Id);
+
+            if (role is null)
+            {
+                return new Role() { Id = 0 };
+            }
             
-        }
-        public Task<Role> CreateRoleAsync(Role roleToCreate)
-        {
-            throw new NotImplementedException();
-        }
+            role.IsActive = true;
+            role.Name = roleToUpdate.Name;
+            _dbContext.Roles.Update(role);
+            await _dbContext.SaveChangesAsync();
+            return role;
 
-        public Task<Role> DeleteRoleAsync(Role roleToDelete)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<RoleViewModel>> GetAllRolesAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Role> UpdateRoleAsync(Role roleToUpdate)
-        {
-            throw new NotImplementedException();
         }
     }
 }
