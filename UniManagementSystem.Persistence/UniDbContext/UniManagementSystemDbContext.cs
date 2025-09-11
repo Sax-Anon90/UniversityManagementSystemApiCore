f0a101f423167164f97e1531e8f9b67a717f1557
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-09-11 09:16:11 +0200

    Add: DbContext for database persistence

---
+++ UniManagementSystem.Persistence/UniDbContext/UniManagementSystemDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Data.DomainEntities;
+
+namespace UniManagementSystem.Persistence.UniDbContext
+{
+    public partial class UniManagementSystemDbContext : DbContext
+    {
+        public UniManagementSystemDbContext()
+        {
+
+        }
+
+        public UniManagementSystemDbContext(DbContextOptions<UniManagementSystemDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<AdminAccount> AdminAccounts { get; set; }
+        public virtual DbSet<AdminAccountRole> AdminAccountRoles { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<CourseCategory> CoursesCategories { get; set; }
+        public virtual DbSet<CourseEnrollment> CourseEnrollments { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }  
+        public virtual DbSet<StudentAccount> StudentAccounts { get; set; }
+    }
+}
