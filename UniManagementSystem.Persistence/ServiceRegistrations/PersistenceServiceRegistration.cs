ad5db7cc3d6092168af8c07bd9bfbcf0926e18ee
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-09-11 12:18:45 +0200

    Complete: Persistence service Registrations

--- UniManagementSystem.Persistence/ServiceRegistrations/PersistenceServiceRegistration.cs
+++ UniManagementSystem.Persistence/ServiceRegistrations/PersistenceServiceRegistration.cs
@@ -6,6 +6,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniManagementSystem.Application.RepositoryInterfaces.AdminAccountRoles;
+using UniManagementSystem.Application.RepositoryInterfaces.AdminAccounts;
+using UniManagementSystem.Application.RepositoryInterfaces.CourseCategories;
+using UniManagementSystem.Application.RepositoryInterfaces.CourseEnrollments;
+using UniManagementSystem.Application.RepositoryInterfaces.Courses;
+using UniManagementSystem.Application.RepositoryInterfaces.Roles;
+using UniManagementSystem.Application.RepositoryInterfaces.StudentAccounts;
+using UniManagementSystem.Persistence.RepositoryImplementations.AdminAccountRoles;
+using UniManagementSystem.Persistence.RepositoryImplementations.AdminAccounts;
+using UniManagementSystem.Persistence.RepositoryImplementations.CourseCategories;
+using UniManagementSystem.Persistence.RepositoryImplementations.Courses;
+using UniManagementSystem.Persistence.RepositoryImplementations.Roles;
+using UniManagementSystem.Persistence.RepositoryImplementations.StudentAccounts;
 using UniManagementSystem.Persistence.UniDbContext;
 
 namespace UniManagementSystem.Persistence.ServiceRegistrations
@@ -16,7 +29,25 @@ namespace UniManagementSystem.Persistence.ServiceRegistrations
         {
             services.AddDbContext<UniManagementSystemDbContext>(options =>
              options.UseSqlite($"Data Source={Path.Combine(AppContext.BaseDirectory, "appdata/UniManagementSystem.db")}"));
+
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var dbContext = serviceProvider.GetRequiredService<DbContext>();
+
+            dbContext.Database.MigrateAsync();
+
+            services.AddScoped<IStudentAccountRepositoryAsync, StudentAccountRepositoryAsync>();
+            services.AddScoped<IRoleRepositoryAsync, RoleRepositoryAsync>();
+            services.AddScoped<ICourseRepositoryAsync, CourseRepositoryAsync>();
+            services.AddScoped<ICourseEnrollmentRepositoryAsync, ICourseEnrollmentRepositoryAsync>();
+            services.AddScoped<ICourseCategoryRepositoryAsync, CourseCategoryRepositoryAsync>();
+            services.AddScoped<IAdminAccountRepositoryAsync, AdminAccountRepositoryAsync>();
+            services.AddScoped<IAdminAccountRoleRepositoryAsync, AdminAccountRoleRepositoryAsync>();
+
             return services;
+
+
         }
     }
 }
