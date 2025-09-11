78a73bc50e5fe7b801f758ada924df6d9f8079a6
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-09-11 11:13:35 +0200

    Complete: StudentAccount Repository

--- UniManagementSystem.Persistence/RepositoryImplementations/StudentAccounts/StudentAccountRepositoryAsync.cs
+++ UniManagementSystem.Persistence/RepositoryImplementations/StudentAccounts/StudentAccountRepositoryAsync.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,38 +7,116 @@ using System.Threading.Tasks;
 using UniManagementSystem.Application.RepositoryInterfaces.StudentAccounts;
 using UniManagementSystem.Data.DomainEntities;
 using UniManagementSystem.Data.DomainModels.ViewModels.StudentAccounts;
+using UniManagementSystem.Persistence.UniDbContext;
 
 namespace UniManagementSystem.Persistence.RepositoryImplementations.StudentAccounts
 {
     public class StudentAccountRepositoryAsync : IStudentAccountRepositoryAsync
     {
-        public StudentAccountRepositoryAsync()
+        private readonly UniManagementSystemDbContext _dbContext;
+        public StudentAccountRepositoryAsync(UniManagementSystemDbContext _dbContext)
         {
-            
+            this._dbContext = _dbContext;
         }
-        public Task<StudentAccount> CreateStudentAccountAsync(StudentAccount studentAccToCreate)
+        public async Task<StudentAccount> CreateStudentAccountAsync(StudentAccount studentAccToCreate)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<StudentAccount> DeleteStudentAccountAsync(StudentAccount studentAccountToRemove)
-        {
-            throw new NotImplementedException();
+            await _dbContext.StudentAccounts.AddAsync(studentAccToCreate);
+            await _dbContext.SaveChangesAsync();
+            return studentAccToCreate;
         }
 
-        public Task<IEnumerable<StudentAccountViewModel>> GetAllStudentAccountsAsync()
+        public async Task<StudentAccount> DeleteStudentAccountAsync(StudentAccount studentAccountToRemove)
         {
-            throw new NotImplementedException();
+            var studentAccount = await _dbContext.StudentAccounts.AsNoTracking()
+                .SingleOrDefaultAsync(x => x.Id == studentAccountToRemove.Id);
+
+            if (studentAccountToRemove is null)
+            {
+                return new StudentAccount() { Id = 0 };
+            }
+
+            studentAccount.IsActive = false;
+            studentAccount.DateInactive = DateTime.UtcNow;
+            _dbContext.StudentAccounts.Update(studentAccountToRemove);
+            await _dbContext.SaveChangesAsync();
+            return studentAccount;
         }
 
-        public Task<StudentAccountViewModel> GetStudentAccountByIdAsync(int studentAccountId)
+        public async Task<IEnumerable<StudentAccountViewModel>> GetAllStudentAccountsAsync()
         {
-            throw new NotImplementedException();
+            var studentAccounts = await _dbContext.StudentAccounts.Select(x => new StudentAccountViewModel()
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Gender = x.Gender,
+                StudentEmail = x.StudentEmail,
+                StudentNumber = x.StudentNumber,
+                IsActive = x.IsActive,
+                DateOfBirth = x.DateOfBirth,
+                DateCreated = x.DateCreated,
+                DateModified = x.DateModified,
+                DateInactive = x.DateInactive
+
+
+            }).ToListAsync();
+
+            if (!studentAccounts.Any())
+            {
+                return Enumerable.Empty<StudentAccountViewModel>();
+            }
+            return studentAccounts;
         }
 
-        public Task<StudentAccount> UpdateStudentAccountAsync(StudentAccount studentAccToUpdate)
+        public async Task<StudentAccountViewModel> GetStudentAccountByIdAsync(int studentAccountId)
         {
-            throw new NotImplementedException();
+            var studentAccount = await _dbContext.StudentAccounts.Select(x => new StudentAccountViewModel()
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Gender = x.Gender,
+                StudentEmail = x.StudentEmail,
+                StudentNumber = x.StudentNumber,
+                IsActive = x.IsActive,
+                DateOfBirth = x.DateOfBirth,
+                DateCreated = x.DateCreated,
+                DateModified = x.DateModified,
+                DateInactive = x.DateInactive
+
+
+            }).SingleOrDefaultAsync(x => x.Id == studentAccountId);
+
+            if (studentAccount is not null)
+            {
+                return new StudentAccountViewModel() { Id = 0 };
+            }
+
+            return studentAccount;
+        }
+
+        public async Task<StudentAccount> UpdateStudentAccountAsync(StudentAccount studentAccToUpdate)
+        {
+            var studentAccount = await _dbContext.StudentAccounts.AsNoTracking()
+                 .SingleOrDefaultAsync(x => x.Id == studentAccToUpdate.Id);
+
+            if (studentAccount is null)
+            {
+                return new StudentAccount() { Id = 0 };
+            }
+
+            studentAccount.FirstName = studentAccToUpdate.FirstName;
+            studentAccount.LastName = studentAccToUpdate.LastName;
+            studentAccount.Gender = studentAccToUpdate.Gender;
+            studentAccount.DateOfBirth = studentAccToUpdate.DateOfBirth;
+            studentAccount.StudentEmail = studentAccToUpdate.StudentEmail;
+            studentAccount.IsActive = studentAccToUpdate.IsActive;
+            studentAccount.StudentNumber = studentAccToUpdate.StudentNumber;
+            studentAccount.DateModified = DateTime.UtcNow;
+
+            _dbContext.StudentAccounts.Update(studentAccount);
+            await _dbContext.SaveChangesAsync();
+            return studentAccount;
         }
     }
 }
