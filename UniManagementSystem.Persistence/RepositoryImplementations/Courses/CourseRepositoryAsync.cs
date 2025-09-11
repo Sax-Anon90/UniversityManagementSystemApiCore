5d444564e019a27ccc278209d621145cf2d4209f
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-09-11 11:27:31 +0200

    Complete: Course Repository

--- UniManagementSystem.Persistence/RepositoryImplementations/Courses/CourseRepositoryAsync.cs
+++ UniManagementSystem.Persistence/RepositoryImplementations/Courses/CourseRepositoryAsync.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,33 +7,78 @@ using System.Threading.Tasks;
 using UniManagementSystem.Application.RepositoryInterfaces.Courses;
 using UniManagementSystem.Data.DomainEntities;
 using UniManagementSystem.Data.DomainModels.ViewModels.Courses;
+using UniManagementSystem.Data.DomainModels.ViewModels.Roles;
+using UniManagementSystem.Persistence.UniDbContext;
 
 namespace UniManagementSystem.Persistence.RepositoryImplementations.Courses
 {
     public class CourseRepositoryAsync : ICourseRepositoryAsync
     {
-        public CourseRepositoryAsync()
+        private readonly UniManagementSystemDbContext _dbContext;
+        public CourseRepositoryAsync(UniManagementSystemDbContext _dbContext)
         {
-            
+            this._dbContext = _dbContext;
         }
-        public Task<Course> CreateCourseAsync(Course courseToCreate)
+        public async Task<Course> CreateCourseAsync(Course courseToCreate)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<Course> DeleteCourseAsync(Course courseToDelete)
-        {
-            throw new NotImplementedException();
+            await _dbContext.Courses.AddAsync(courseToCreate);
+            await _dbContext.SaveChangesAsync();
+            return courseToCreate;
         }
 
-        public Task<IEnumerable<CourseViewModel>> GetAllCoursesAsync()
+        public async Task<Course> DeleteCourseAsync(Course courseToDelete)
         {
-            throw new NotImplementedException();
+            var course = await _dbContext.Courses.AsNoTracking()
+                .SingleOrDefaultAsync(x => x.Id == courseToDelete.Id);
+
+            if (course is null)
+            {
+                return new Course() { Id = 0 };
+            }
+
+            course.IsActive = false;
+            course.DateInactive = courseToDelete.DateInactive;
+            _dbContext.Courses.Update(course);
+            await _dbContext.SaveChangesAsync();
+
+            return course;
+
         }
 
-        public Task<Course> UpdateCourseAsync(Course courseToUpdate)
+        public async Task<IEnumerable<CourseViewModel>> GetAllCoursesAsync()
         {
-            throw new NotImplementedException();
+            var courses = await _dbContext.Roles.Select(x => new CourseViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                IsActive = x.IsActive,
+                DateCreated = x.DateCreated,
+                DateModified = x.DateModified,
+                DateInactive = x.DateInactive,
+            }).ToListAsync();
+
+            if (!courses.Any())
+            {
+                return Enumerable.Empty<CourseViewModel>();
+            }
+
+            return courses;
+        }
+
+        public async Task<Course> UpdateCourseAsync(Course courseToUpdate)
+        {
+            var course = await _dbContext.Courses.AsNoTracking().SingleOrDefaultAsync(x => x.Id == courseToUpdate.Id);
+
+            if (course is null)
+            {
+                return new Course() { Id = 0 };
+            }
+
+            course.IsActive = true;
+            course.Name = courseToUpdate.Name;
+            _dbContext.Courses.Update(course);
+            await _dbContext.SaveChangesAsync();
+            return course;
         }
     }
 }
